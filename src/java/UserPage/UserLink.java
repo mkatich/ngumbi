d96da5b2a164fe69ec5fda1a63fa815e9b1e6e73
mkatich <mkatich@gmail.com>
2021-05-27 16:45:40 -0500

    Added new classes UserLink and UserPageDAO, just placeholders for now

---
+++ src/java/UserPage/UserLink.java
@@ -0,0 +1,16 @@
+/*
+ * UserLink class is used to create objects for a user's links based on entries 
+ * in the user_links table. 
+ * This does not retrieve any data related to these objects - that must be 
+ * done by other code (UserPageDAO) and then it can use this class to
+ * create and use these objects.
+ */
+package UserPage;
+
+/**
+ *
+ * @author Michael
+ */
+public class UserLink {
+    
+}
---
+++ src/java/UserPage/UserPageDAO.java
@@ -0,0 +1,14 @@
+/*
+ * UserPageDAO class serves as the data access object for the user's page. 
+ * To do this, it pulls various data from the users and user_links tables. 
+ * 
+ */
+package UserPage;
+
+/**
+ *
+ * @author Michael
+ */
+public class UserPageDAO {
+    
+}
